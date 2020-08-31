0d124e6ada6ca3fc3a769305c83770222f468822
gopic <gopic@192.168.100.6>
2020-08-31 22:16:30 +0530

    Added class TestGitby

---
+++ StudentWebProject_Spring_Hibernate/src/com/dxc/testGIT/TestGitbyGopi.java
@@ -0,0 +1,11 @@
+package com.dxc.testGIT;
+
+public class TestGitbyGopi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Git is fun!!");
+
+	}
+
+}
